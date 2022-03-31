ce8e3faf0051f983ef6baca679091bd38f0a5ad7
root <tt_123@123.com>
2022-03-31 18:43:26 +0800

    第二次提交本地库  修改testcontroller

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -1,4 +1,10 @@
 package cn.tt.git.controller;
-
+//绿色   提交到暂存区
+//红色  未提交
+//蓝色   被修改过
+//正常颜色  已提交
 public class TestController {
-}
+    public static  void  main(String[] args){
+        System.out.println("hello  git");
+
+    }}
